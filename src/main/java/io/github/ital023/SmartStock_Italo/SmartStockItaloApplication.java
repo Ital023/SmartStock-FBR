630b40ddd220bb8a494e6eddbee9aedae2d7365a
Ital023 <contadoitalo911@gmail.com>
2025-06-07 23:49:12 -0300

    feat: initial files

---
+++ src/main/java/io/github/ital023/SmartStock_Italo/SmartStockItaloApplication.java
@@ -0,0 +1,13 @@
+package io.github.ital023.SmartStock_Italo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartStockItaloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartStockItaloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/ital023/SmartStock_Italo/SmartStockItaloApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ital023.SmartStock_Italo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartStockItaloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
