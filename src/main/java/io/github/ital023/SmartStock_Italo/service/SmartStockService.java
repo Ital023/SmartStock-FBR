d4da50fd6c1add3e879f6421ec9ce602e61c9026
Ital023 <contadoitalo911@gmail.com>
2025-06-09 00:31:35 -0300

    feat: regra de negocio para recompra dos itens

--- src/main/java/io/github/ital023/SmartStock_Italo/service/SmartStockService.java
+++ src/main/java/io/github/ital023/SmartStock_Italo/service/SmartStockService.java
@@ -1,5 +1,6 @@
 package io.github.ital023.SmartStock_Italo.service;
 
+import io.github.ital023.SmartStock_Italo.domain.CsvStockItem;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -18,11 +19,23 @@ public class SmartStockService {
         try {
             var items = reportService.readStockReport(reportPath);
 
+            items.forEach(item -> {
+
+                if(item.getQuantity() < item.getReorderThreshold()) {
+                    var reorderQuantity = calculateReorderQuantity(item);
+                }
+
+            });
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
 
+    }
+
+    private Integer calculateReorderQuantity(CsvStockItem item) {
+        return item.getReorderThreshold() + ((int) Math.ceil(item.getReorderThreshold() * 0.2));
     }
 
 
